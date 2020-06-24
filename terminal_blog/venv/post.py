f9dcd65fbef6eab25f99dc31fb499ed31d92fdb8
Bernardino Sassoli de' Bianchi <61242474+bsassoli@users.noreply.github.com>
2020-06-25 01:10:24 +0200

    New blog app in MongoDB
    Created project for blog app

---
+++ terminal_blog/venv/post.py
@@ -0,0 +1,8 @@
+__author__ = 'bernardino'
+class Post():
+    def __init__(self):
+        self.title = 'This is my title'
+        self.content = 'Some content'
+        self.author = 'Bernardino'
+post1 = Post()
+post2 = Post()
