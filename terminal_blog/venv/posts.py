c943a32028ec7a98a80bbf49bbb1a36c8a2caa93
Bernardino Sassoli de' Bianchi <61242474+bsassoli@users.noreply.github.com>
2020-06-27 19:59:39 +0200

    Update posts.py
    Added methods

--- terminal_blog/venv/posts.py
+++ terminal_blog/venv/posts.py
@@ -1,14 +1,15 @@
 __author__ = 'bernardino'
-
+import uuid
+from database import Database
 
 class Post():
-    def __init__(self, post_id, title, content, author, date_created, blog_id ='This blog'):
+    def __init__(self, title, content, author, date_created, blog_id ='This blog', post_id=None):
         self.blog_id = blog_id
-        self.post_id = post_id
+        self.post_id = uuid.uuid4().hex if post_id is None else post_id
         self.title = title
+        self.date_created = date
         self.content = content
         self.author = author
-        self.date_created = date_created
 
     def save_to_mongo(self):
         pass
@@ -24,3 +25,11 @@ class Post():
             'content': self.author,
             'date_created': self.date_created
         }
+
+    @staticmethod
+    def from_mongo(id):
+        data = Database.find_one(collection='posts', query={'id':id})
+
+    @staticmethod
+    def from_blog(id):
+        return [post for post in Database.find(collection='posts', query={'blog_id':id})]
